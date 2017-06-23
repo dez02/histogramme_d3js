85fd2856a76f00cfc281a846fc1130db96504103
Désirée <deutodesiree@gmail.com>
2017-06-23 12:13:20 +0200

    added 3 files

---
+++ histo.js
@@ -0,0 +1,26 @@
+$.getJSON('https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json', function(data){
+	var dataset = data.data;
+	console.log(data.data);
+	
+
+var svgWidth = 800;
+var svgHeight = 500;
+
+// Create svg
+var svg = d3.select("body")
+            .append("svg")
+            .attr("width", svgWidth)
+            .attr("height", svgHeight);
+
+
+
+
+
+
+
+            
+	
+});
+
+								
+
