53a4e91902f69a4b20ceaaf7ab3931c6a44f4e09
Désirée <deutodesiree@gmail.com>
2017-06-23 12:20:32 +0200

    create xScale yScale and rScale

--- histo.js
+++ histo.js
@@ -5,12 +5,36 @@ $.getJSON('https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/m
 
 var svgWidth = 800;
 var svgHeight = 500;
+var padding = 40;
+
+var xScale = d3.scaleLinear()
+               .domain([0, d3.max(dataset,(d) => d[0])])
+               .range([padding, svgWidth - padding * 2]);
+
+var yScale = d3.scaleLinear()
+               .domain([0, d3.max(dataset, (d) => d[1])])
+               .range([svgHeight - padding, padding]); 
+
+var rScale = d3.scaleLinear()
+              .domain([0, d3.max(dataset,(d)=> d[1])])
+              .range([2,5]);
 
 // Create svg
 var svg = d3.select("body")
             .append("svg")
             .attr("width", svgWidth)
             .attr("height", svgHeight);
+
+svg.selectAll("rect")
+   .data(dataset)
+   .enter()
+   .append("rect")
+   .attr("cx", (d)=> xScale(d[0]))
+   .attr("cy", (d)=> yScale(d[1]))
+   .attr("r", (d)=> rScale(d[1]));
+
+
+
 	
 });
 
