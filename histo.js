137f2c7c80d92d39a240d95e9556d8fc783d28a1
Désirée <deutodesiree@gmail.com>
2017-06-26 14:11:27 +0200

    updated histo.js

--- histo.js
+++ histo.js
@@ -1,23 +1,25 @@
 $.getJSON('https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json', function(data){
-	var dataset = data.data;
-	console.log(data.data);
-	
+	var dataset = data.data;	
 
 var svgWidth = 800;
 var svgHeight = 500;
 var padding = 40;
+var barPadding = 2;
 
-var xScale = d3.scaleLinear()
-               .domain([0, d3.max(dataset,(d) => d[0])])
-               .range([padding, svgWidth - padding * 2]);
 
+// Define the x scale
+var mindate = new Date(1947,0,1),
+    maxdate = new Date(2015,06,01);
+            
+var xScale = d3.scaleTime()
+	      		.domain([mindate, maxdate])    
+				.range([padding, svgWidth - padding * 2]);   
+		    
+// yScale
 var yScale = d3.scaleLinear()
                .domain([0, d3.max(dataset, (d) => d[1])])
                .range([svgHeight - padding, padding]); 
 
-var rScale = d3.scaleLinear()
-              .domain([0, d3.max(dataset,(d)=> d[1])])
-              .range([2,5]);
 
 // Create svg
 var svg = d3.select("body")
@@ -29,13 +31,15 @@ svg.selectAll("rect")
    .data(dataset)
    .enter()
    .append("rect")
-   .attr("cx", (d)=> xScale(d[0]))
-   .attr("cy", (d)=> yScale(d[1]))
-   .attr("r", (d)=> rScale(d[1]));
+   .attr("x", (d)=> xScale(Date.parse(d[0])))
+   .attr("y", (d)=> yScale(d[1]))
+   .attr("width", svgWidth / dataset.length - barPadding)
+   .attr("height", (d)=> (svgHeight - padding - yScale(d[1])))
+   .attr("fill", "red");
 
-    // Create X axis
+// Create X axis
 var xAxis = d3.axisBottom(xScale)
-              // .ticks(5);
+              .ticks(12);
 
 svg.append("g")
                .attr("class","axis")
@@ -44,17 +48,13 @@ svg.append("g")
 
 //Create Y axis
 var yAxis = d3.axisLeft(yScale)
-                // .ticks(5);
-                console.log(yAxis);
+                .ticks(10);
 
 svg.append("g")
     .attr("class", "axis")
     .attr("transform", "translate(" + padding + ",0)")
     .call(yAxis);
 
-
-
-	
 });
 
 								
